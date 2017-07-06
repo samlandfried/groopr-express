f48714315b32f2e128e5026ce39bc104587a8ec7
Sam Landfried <samlandfried@gmail.com>
2017-07-06 15:56:09 -0600

    Add bodyParser

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.locals.title = 'Groopr';
 
 if (!module.parent) {
   app.set('port', process.env.PORT || 3000);
   app.listen(app.get('port'), () => console.log(`${app.locals.title} is running on port ${app.get('port')}.`))
 }
+
+app.get('/groop', (request, response) => {
+  body = {
+    groups: [0,1,2,3,4,5,6,7,8,9]
+  }
+  response.json(body);
+});
 
 module.exports = app;
