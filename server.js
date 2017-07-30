0fa11b4e2a7c7c48e9790133c681b5d54be00d0a
Sam Landfried <samlandfried@gmail.com>
2017-07-29 20:34:59 -0600

    Moved to production, switched to PG

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const namespace = require('express-namespace');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const Grouper = require('./lib/grouper')
 const groupsController = require('./lib/controllers/groupsController');
 const GroupsController = new groupsController();
 
 const app = express();
 
+app.use(cors({origin: 'http://localhost:3000'}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -19,7 +22,7 @@ app.use(function(req, res, next) {
 app.locals.title = 'Groopr';
 
 if (!module.parent) {
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3001);
   app.listen(app.get('port'), () => console.log(`${app.locals.title} is running on port ${app.get('port')}.`));
 }
 
